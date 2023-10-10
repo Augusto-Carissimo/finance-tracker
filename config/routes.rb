688d50880234a6333fe42e3f431e8208d959f693
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-10-10 11:24:58 -0300

    Add search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
   get 'my_portofolio', to: 'users#my_portofolio'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "welcome#index"
+  get 'search_stock', to: 'stocks#search'
 end
