21f74cec58e9829124d3328cf0dd7dde876cd803
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-10-11 14:31:20 -0300

    Add UserStock functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   get 'stocks/search'
-  get 'my_portofolio', to: 'users#my_portofolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
