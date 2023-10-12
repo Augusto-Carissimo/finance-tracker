5c0d026e5e40bc5587c31e248550120c9084b1d5
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-10-12 07:52:12 -0300

    Refactor Search with turbo_stream

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,10 @@
 class StocksController < ApplicationController
   def search
-    @tracked_stocks = current_user.stocks
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        # render 'users/my_portfolio'
+        update_search
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
@@ -13,5 +13,12 @@ class StocksController < ApplicationController
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
     end
+  end
+
+  private
+
+  def update_search
+    render turbo_stream:
+    turbo_stream.replace('results_turbo_stream', partial: 'users/results')
   end
 end
