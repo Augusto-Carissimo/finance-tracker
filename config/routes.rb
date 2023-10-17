595e0a8975b8cf85539461f98cde7f4cc97cb39a
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-10-17 10:17:30 -0300

    Add remove stock from portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root "welcome#index"
   get 'search_stock', to: 'stocks#search'
+
 end
